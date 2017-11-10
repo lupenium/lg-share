5813c587b93b7b493a117e0959cc20dcaaadfcbf
Luiz Pereira <luiz.tnk@gmail.com>
2017-11-10 10:50:59 -0200

    Refactories and Dist

--- dist/js/lg-share.js
+++ dist/js/lg-share.js
@@ -1,3 +1,23 @@
+/*! lg-share - v1.1.1 - 2017-11-10
+* http://sachinchoolur.github.io/lightGallery
+* Copyright (c) 2017 Sachin N; Licensed GPLv3 */
+
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module unless amdModuleId is set
+    define(['jquery'], function (a0) {
+      return (factory(a0));
+    });
+  } else if (typeof module === 'object' && module.exports) {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory(require('jquery'));
+  } else {
+    factory(root["jQuery"]);
+  }
+}(this, function ($) {
+
 (function() {
     'use strict'
 
@@ -164,3 +184,6 @@
 
 })();
 
+
+
+}));
